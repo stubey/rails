aef3743500f6edaa1049c110278cd14b1bd6dc9e
Tom Stubitsch <tom.stubitsch@sbcglobal.net>
2011-10-27 22:48:38 -0500

    Initial commit

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '879c045bf12b937ee0a9551f60e9e3ec5cba201cc49e5c6aa8ed50164fa78ffecc87d6705862b0ce17e6aaea2b26b65d48924b4110ad9384ff239b0b7c6fd694'
---
+++ first_app/config/scum.rb
@@ -0,0 +1,5 @@
+puts __FILE__
+puts File.expand_path(__FILE__)
+puts File.expand_path('../application')
+puts File.expand_path('../application', __FILE__)
+
